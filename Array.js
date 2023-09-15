60bed3a72bba3bf0e722d85d9955b895e702af96
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-09-15 14:34:50 +0530

    Learn java

---
+++ Array.js
@@ -0,0 +1,45 @@
+// //===Arrays===(An array is a special variable, which can hold more than one value)
+
+// let book1 = "Maths";
+// let book2 = "Physic";
+// let book3 = "Chemistry";
+// let book4 = "bio";
+// let book5 = "Anantomy";
+
+// console.log(book3);
+
+//==Arrays Pratices==(Arrays )
+
+//==push==
+
+// let Vegies = ["Leadyfinger", "Tomato", "Potato", "green-chilli"];
+
+// Vegies.push("Peas", "Carrot");
+
+// console.log(Vegies);
+
+// //==pop== ( pop = Last element is remove )
+
+// let fruits = ["Mango", "Graps", "apple"];
+
+// fruits.pop();
+
+// // console.log(fruits);
+
+// fruits.unshift("Hindi", "English"); //(unshift = (Add new elements to an array)) Add some one or more element in the staring part of array (Add new elements to an array))
+
+//==splice
+
+// fruits.splice(1, 2);
+
+// fruits.length = 0;
+
+// console.log(fruits);
+
+//==Indexof
+
+let book = ["maths", "physic", "chemistry"];
+
+let position = book.indexOf(1, 3);
+
+console.log(position);
