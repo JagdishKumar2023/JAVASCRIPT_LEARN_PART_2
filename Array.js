e9e0962394931d42b1386cfba950fe90b065866b
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-09-16 13:19:21 +0530

    New Changes

--- Array.js
+++ Array.js
@@ -38,8 +38,68 @@
 
 //==Indexof
 
-let book = ["maths", "physic", "chemistry"];
+// let book = ["maths", "physic", "chemistry"];
 
-let position = book.indexOf(1, 3);
+// let position = book.indexOf(2);
 
-console.log(position);
+// console.log(position);
+
+//==(Split)(space between Two string)
+
+// let text = "This is a  random text";
+
+// let wordarray = text.split("-");
+
+// console.log(text);
+
+//==(WithPages)
+
+// let book = ["Math", "Physics", "bio", "Computer Science"];
+
+// let bookWithPages = [[("Math", "400")], [("Physic", "500")], [("Bio", "600")]];
+
+// let fetch = bookWithPages[0];
+
+// console.log(fetch);
+
+// let movies = ["Pushpa", "RRR", "kgf"];
+
+// let actormovies = [
+//   [("allu-arjun", "Pushpa")],
+//   [("ram charen", "RRR")],
+//   [("yash", "kgf")],
+// ];
+
+// actormovies[0];
+
+// console.log(actormovies);
+
+// for (i = 0; i < actormovies; i++) {
+//   console.log(`Element ${i} is ${movies[i]} \n`);
+// }
+
+// console.log(actormovies);
+
+//===Parameters and Argument (funtion)
+
+// function MulTable(num) {
+//   for (i = 1; i <= 10; i++) {
+//     console.log(`${num} x ${num * i}`);
+//   }
+// }
+
+// MulTable(4);
+
+// function add(sum) {
+//   for (i = 1; i <= 10; i++) {
+//     console.log(`${sum} + ${sum + i}`);
+//   }
+// }
+
+// add(1);
+
+// function divide(num1, num2) {
+//   console.log(num1 / num2);
+// }
+
+// divide(20, 2);
