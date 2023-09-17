fcfba86707d5e48ead103fa1b378477ed07c1c86
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-09-17 12:43:58 +0530

    New Changes

---
+++ Arguments.js
@@ -0,0 +1,44 @@
+// function add() {
+//   if (arguments.length == 0) {
+//     console.log("No parameter passed !");
+//   } else {
+//     let sum = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//       sum = sum + arguments[i];
+//     }
+//     console.log(sum);
+//   }
+// }
+
+// let addition = add;
+
+// // addition(5, 2);
+// add(5, 7, 10);
+
+//===RETURN VALUE IN JAVASCRIPT
+
+// function compare(a, b) {
+//   if (a > b) {
+//     return 10;
+//   } else if (b > a) {
+//     return -10;
+//   } else {
+//     return 0;
+//   }
+// }
+
+// let c = compare(12, 11);
+
+// console.log(c);
+
+// function compare(a, b) {
+//   if (a > b) {
+//     return true;
+//   } else if (b > a) {
+//     return false;
+//   }
+// }
+
+// let c = compare(5, 12);
+
+// console.log(c);
