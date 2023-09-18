dc527432714e7d46e34d9a453d698d7759193819
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-09-18 13:55:04 +0530

    New Changes

---
+++ Condition.js
@@ -0,0 +1,20 @@
+// let age = 19;
+// let hasVoterCard = "No";
+
+// if (age >= 14 && hasVoterCard == "Yes") {
+//   alert("You Can vote!");
+// } else {
+//   alert("You cannot vote!");
+// }
+
+let isLoggedin = 1; // if 0 loggedout if 1 login
+
+if (isLoggedin == 0) {
+  document.write("Login");
+} else {
+  document.write("Logout");
+}
+
+// let option = isLoggedin == 1 ? "Logout" : "Login";
+// // condition                     false         true
+// document.write(option);
