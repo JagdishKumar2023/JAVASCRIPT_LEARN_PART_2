4458c2ee3b39992f4e53828cade5bedf8678e96e
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-09-24 14:03:59 +0530

    New Changes

---
+++ high_order_part2.js
@@ -0,0 +1,91 @@
+//===MAP===(HOTEL DATA FOR VEG AND NON-VEG PRICE  )
+
+let ZomatoMenu = [
+  { id: 1, NonVeg: "chic masala plate", Price: 140, count: 8 },
+  { id: 2, NonVeg: "Harabhara Kabaab", Price: 360, count: 10 },
+  { id: 3, Snack: "Fish Tikka", Price: 380, count: 7 },
+  { id: 4, complimentry: "Dal snack", Price: 0, count: "Unlimited" },
+  { id: 5, Veg: "Veg Biryani", Price: 140, count: 12 },
+  { id: 6, Snack: "Cripy Corn", Price: 100, count: 15 },
+  { id: 7, Papad: "Papad Masala,Fry Papad", Price: 20, count: 40 },
+];
+
+// update
+
+// let NewZomatoMenu = ZomatoMenu.map(function (ele, idx, array) {
+//   return {
+//     ...ele,
+//     count: ele.count - 7,
+//   };
+// });
+
+// console.log(NewZomatoMenu);
+
+// let NewZomatoMenu = ZomatoMenu.map(function (ele, idx, array) {
+//   return {
+//     ...ele,
+//     Price: ele.Price + 30,
+//   };
+// });
+
+// console.log(NewZomatoMenu);
+
+//worng code dout
+
+// let discount_Snack = ZomatoMenu.map(function (ele, idx, array) {
+//   return {
+//     ...ele,
+//     Snack: ele.Snack - 40,
+//   };
+// });
+
+// console.log(discount_Snack);
+
+// let discount_Snack = ZomatoMenu.map(function (ele, idx, array) {
+//   if (ele.Snack === "Fish Tikka") {
+//     return {
+//       ...ele,
+//       Price: ele.Price - 50,
+//     };
+//   }
+// });
+
+// console.log(discount_Snack);
+
+//=== SCOTCH DATA
+
+let scotch_data = [
+  { id: 1, scotch: "HUNDRED Pipers", price: 1400, total_sale: 8 },
+  { id: 2, scotch: "Belantin", price: 1700, total_sale: 12 },
+  { id: 3, scotch: "Black and White", price: 1870, total_sale: 15 },
+  { id: 4, scotch: "Redlabel", price: 2150, total_sale: 13 },
+];
+
+// let Newscotch_data = scotch_data.map(function (ele, idx, array) {
+//   return {
+//     ...ele,
+//     price: ele.price - 100,
+//   };
+// });
+
+// console.log(Newscotch_data);
+
+// let Newscotch_data = scotch_data.map(function (ele, idx, array) {
+//   return {
+//     ...ele,
+//     total_sale: ele.total_sale - 4,
+//   };
+// });
+
+// console.log(Newscotch_data);
+
+let discount_Redlabel = scotch_data.map(function (ele, id, array) {
+  if (ele.scotch === "Redlabel") {
+    return {
+      ...ele,
+      price: ele.price - 150,
+    };
+  }
+});
+
+console.log(discount_Redlabel);
--- hof_practice.js
+++ hof_practice.js
@@ -2,36 +2,36 @@
 // To show data on website
 // to update some value of data
 
-let product_data = [
-  { id: 1, product: "tshirt", price: 500, count: 10 },
-  { id: 2, product: "shirt", price: 800, count: 10 },
-  { id: 3, product: "jeans", price: 1500, count: 10 },
-  { id: 4, product: "shorts", price: 1200, count: 10 },
-  { id: 5, product: "lower", price: 2000, count: 10 },
-  { id: 6, product: "shoes", price: 2500, count: 10 },
-  { id: 7, product: "watch", price: 4500, count: 10 },
-];
+// let product_data = [
+//   { id: 1, product: "tshirt", price: 500, count: 10 },
+//   { id: 2, product: "shirt", price: 800, count: 10 },
+//   { id: 3, product: "jeans", price: 1500, count: 10 },
+//   { id: 4, product: "shorts", price: 1200, count: 10 },
+//   { id: 5, product: "lower", price: 2000, count: 10 },
+//   { id: 6, product: "shoes", price: 2500, count: 10 },
+//   { id: 7, product: "watch", price: 4500, count: 10 },
+// ];
 
 // update
 
-let new_discounted_data = product_data.map(function (ele, idx, array) {
-  return {
-    ...ele,
-    price: ele.price - 100,
-  };
-});
+// let new_discounted_data = product_data.map(function (ele, idx, array) {
+//   return {
+//     ...ele,
+//     price: ele.price - 100,
+//   };
+// });
 
 // console.log(new_discounted_data);
 
-let discounted_shoes = product_data.map(function (ele, idx, array) {
-  if (ele.product === "shoes") {
-    return {
-      ...ele,
-      price: ele.price - 100,
-    };
-  }
-  return ele;
-});
+// let discounted_shoes = product_data.map(function (ele, idx, array) {
+//   if (ele.product === "shoes") {
+//     return {
+//       ...ele,
+//       price: ele.price - 100,
+//     };
+//   }
+//   return ele;
+// });
 
 // console.log(discounted_shoes);
 
@@ -39,19 +39,19 @@ let discounted_shoes = product_data.map(function (ele, idx, array) {
 
 // returns array
 
-let product_lessthan_thousand = product_data.filter(function (ele, idx, array) {
-  if (ele.price <= 1000) {
-    return ele;
-  }
-});
+// let product_lessthan_thousand = product_data.filter(function (ele, idx, array) {
+//   if (ele.price <= 1000) {
+//     return ele;
+//   }
+// });
 
-console.log(product_lessthan_thousand);
+// console.log(product_lessthan_thousand);
 
-// reduce returns number
-// calculation
+// // reduce returns number
+// // calculation
 
-let total_price = product_data.reduce(function (acc, ele, array) {
-  return acc + ele.price;
-}, 0);
+// let total_price = product_data.reduce(function (acc, ele, array) {
+//   return acc + ele.price;
+// }, 0);
 
-console.log(total_price);
+// console.log(total_price);
