8c703f5893baa205b0f1cb214fc2cca29804f129
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-09-24 20:03:49 +0530

    New changes

--- high_order_part2.js
+++ high_order_part2.js
@@ -1,14 +1,14 @@
 //===MAP===(HOTEL DATA FOR VEG AND NON-VEG PRICE  )
 
-let ZomatoMenu = [
-  { id: 1, NonVeg: "chic masala plate", Price: 140, count: 8 },
-  { id: 2, NonVeg: "Harabhara Kabaab", Price: 360, count: 10 },
-  { id: 3, Snack: "Fish Tikka", Price: 380, count: 7 },
-  { id: 4, complimentry: "Dal snack", Price: 0, count: "Unlimited" },
-  { id: 5, Veg: "Veg Biryani", Price: 140, count: 12 },
-  { id: 6, Snack: "Cripy Corn", Price: 100, count: 15 },
-  { id: 7, Papad: "Papad Masala,Fry Papad", Price: 20, count: 40 },
-];
+// let ZomatoMenu = [
+//   { id: 1, NonVeg: "chic masala plate", Price: 140, count: 8 },
+//   { id: 2, NonVeg: "Harabhara Kabaab", Price: 360, count: 10 },
+//   { id: 3, Snack: "Fish Tikka", Price: 380, count: 7 },
+//   { id: 4, complimentry: "Dal snack", Price: 0, count: "Unlimited" },
+//   { id: 5, Veg: "Veg Biryani", Price: 140, count: 12 },
+//   { id: 6, Snack: "Cripy Corn", Price: 100, count: 15 },
+//   { id: 7, Papad: "Papad Masala,Fry Papad", Price: 20, count: 40 },
+// ];
 
 // update
 
@@ -61,6 +61,15 @@ let scotch_data = [
   { id: 4, scotch: "Redlabel", price: 2150, total_sale: 13 },
 ];
 
+// let Newtotal_sale = scotch_data.map(function (ele, idx, array) {
+//   return {
+//     ...ele,
+//     total_sale: ele.total_sale - 5,
+//   };
+// });
+
+// console.log(Newtotal_sale);
+
 // let Newscotch_data = scotch_data.map(function (ele, idx, array) {
 //   return {
 //     ...ele,
@@ -79,13 +88,35 @@ let scotch_data = [
 
 // console.log(Newscotch_data);
 
-let discount_Redlabel = scotch_data.map(function (ele, id, array) {
-  if (ele.scotch === "Redlabel") {
-    return {
-      ...ele,
-      price: ele.price - 150,
-    };
-  }
-});
+// let discount_Redlabel = scotch_data.map(function (ele, idx, array) {
+//   if (ele.scotch === "Redlabel") {
+//     return {
+//       ...ele,
+//       price: ele.price - 150,
+//     };
+//   }
+// });
 
-console.log(discount_Redlabel);
+// let discount_Black = scotch_data.map(function (ele, idx, array) {
+//   if (ele.scotch === "Black and White") {
+//     return {
+//       ...ele,
+//       price: ele.price - 170,
+//     };
+//   }
+// });
+
+// console.log(discount_Black);
+
+// console.log(discount_Redlabel);
+
+// let discount_HUNDRED = scotch_data.map(function (ele, idx, array) {
+//   if (ele.scotch === "HUNDRED Pipers") {
+//     return {
+//       ...ele,
+//       price: ele.price - 140,
+//     };
+//   }
+// });
+
+// console.log(discount_HUNDRED);
