89bdad3f3c4a3e6b1fc7609096a5eb708ab55f87
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-09-25 13:24:46 +0530

    New changes

--- high_order_part2.js
+++ high_order_part2.js
@@ -1,14 +1,14 @@
 //===MAP===(HOTEL DATA FOR VEG AND NON-VEG PRICE  )
 
-// let ZomatoMenu = [
-//   { id: 1, NonVeg: "chic masala plate", Price: 140, count: 8 },
-//   { id: 2, NonVeg: "Harabhara Kabaab", Price: 360, count: 10 },
-//   { id: 3, Snack: "Fish Tikka", Price: 380, count: 7 },
-//   { id: 4, complimentry: "Dal snack", Price: 0, count: "Unlimited" },
-//   { id: 5, Veg: "Veg Biryani", Price: 140, count: 12 },
-//   { id: 6, Snack: "Cripy Corn", Price: 100, count: 15 },
-//   { id: 7, Papad: "Papad Masala,Fry Papad", Price: 20, count: 40 },
-// ];
+let ZomatoMenu = [
+  { id: 1, NonVeg: "chic masala plate", Price: 140, count: 8 },
+  { id: 2, NonVeg: "Harabhara Kabaab", Price: 360, count: 10 },
+  { id: 3, Snack: "Fish Tikka", Price: 380, count: 7 },
+  // { id: 4, complimentry: "Dal snack", Price: 0, count: "Unlimited" },
+  { id: 5, Veg: "Veg Biryani", Price: 140, count: 12 },
+  { id: 6, Snack: "Cripy Corn", Price: 100, count: 15 },
+  { id: 7, Papad: "Papad Masala,Fry Papad", Price: 20, count: 40 },
+];
 
 // update
 
@@ -52,13 +52,14 @@
 
 // console.log(discount_Snack);
 
-//=== SCOTCH DATA
+// === SCOTCH DATA
 
 let scotch_data = [
   { id: 1, scotch: "HUNDRED Pipers", price: 1400, total_sale: 8 },
   { id: 2, scotch: "Belantin", price: 1700, total_sale: 12 },
   { id: 3, scotch: "Black and White", price: 1870, total_sale: 15 },
   { id: 4, scotch: "Redlabel", price: 2150, total_sale: 13 },
+  { id: 5, scotch: "Black Dog", price: 2420, total_sale: 6 },
 ];
 
 // let Newtotal_sale = scotch_data.map(function (ele, idx, array) {
@@ -97,6 +98,8 @@ let scotch_data = [
 //   }
 // });
 
+// console.log(discount_Redlabel);
+
 // let discount_Black = scotch_data.map(function (ele, idx, array) {
 //   if (ele.scotch === "Black and White") {
 //     return {
@@ -108,7 +111,16 @@ let scotch_data = [
 
 // console.log(discount_Black);
 
-// console.log(discount_Redlabel);
+// let discount_blenten = scotch_data.map(function (ele, idx, array) {
+//   if (ele.scotch === "Belantin") {
+//     return {
+//       ...ele,
+//       price: ele.price - 170,
+//     };
+//   }
+// });
+
+// console.log(discount_blenten);
 
 // let discount_HUNDRED = scotch_data.map(function (ele, idx, array) {
 //   if (ele.scotch === "HUNDRED Pipers") {
@@ -120,3 +132,79 @@ let scotch_data = [
 // });
 
 // console.log(discount_HUNDRED);
+
+//===FILTER (Roof chhatana)
+
+// let lessThanEighteen = scotch_data.filter(function (ele, idx, array) {
+//   if (ele.price <= 1800) {
+//     return ele;
+//   }
+// });
+
+// console.log(lessThanEighteen);
+
+// let lessThanTwelve = scotch_data.filter(function (ele, idx, array) {
+//   if (ele.price <= 1400) {
+//     return ele;
+//   }
+// });
+
+// console.log(lessThanTwelve);
+
+// let lessThanHundred = scotch_data.filter(function (ele, idx, array) {
+//   if (ele. <= 1500) {
+//     return ele;
+//   }
+// });
+
+// console.log(lessThanHundred);
+
+// let lessThanTwoThousand = scotch_data.filter(function (ele, idx, array) {
+//   if (ele.price <= 2000) {
+//     return ele;
+//   }
+// });
+
+// console.log(lessThanTwoThousand);
+
+// let TotalSaleLessThanten = scotch_data.filter(function (ele, idx, array) {
+//   if (ele.total_sale <= 10) {
+//     return ele;
+//   }
+// });
+
+// console.log(TotalSaleLessThanten);
+
+// let TotalSaleLessThanFifteen = scotch_data.filter(function (ele, idx, array) {
+//   if (ele.total_sale <= 15) {
+//     return ele;
+//   }
+// });
+
+// console.log(TotalSaleLessThanFifteen);
+
+//===REDUCE (for calculation only)
+
+// let total_sale = scotch_data.reduce(function (acc, ele, array) {
+//   return acc + ele.total_sale;
+// }, 0);
+
+// console.log(total_sale);
+
+// let total_price = scotch_data.reduce(function (acc, ele, idx, array) {
+//   return acc + ele.price;
+// }, 0);
+
+// console.log(total_price);
+
+// let total_price = ZomatoMenu.reduce(function (acc, ele, idx, array) {
+//   return acc + ele.Price;
+// }, 0);
+
+// console.log(total_price);
+
+let total_count = ZomatoMenu.reduce(function (acc, ele, idx, array) {
+  return acc + ele.count;
+}, 0);
+
+console.log(total_count);
--- hof_practice.js
+++ hof_practice.js
@@ -2,15 +2,15 @@
 // To show data on website
 // to update some value of data
 
-// let product_data = [
-//   { id: 1, product: "tshirt", price: 500, count: 10 },
-//   { id: 2, product: "shirt", price: 800, count: 10 },
-//   { id: 3, product: "jeans", price: 1500, count: 10 },
-//   { id: 4, product: "shorts", price: 1200, count: 10 },
-//   { id: 5, product: "lower", price: 2000, count: 10 },
-//   { id: 6, product: "shoes", price: 2500, count: 10 },
-//   { id: 7, product: "watch", price: 4500, count: 10 },
-// ];
+let product_data = [
+  { id: 1, product: "tshirt", price: 500, count: 10 },
+  { id: 2, product: "shirt", price: 800, count: 10 },
+  { id: 3, product: "jeans", price: 1500, count: 10 },
+  { id: 4, product: "shorts", price: 1200, count: 10 },
+  { id: 5, product: "lower", price: 2000, count: 10 },
+  { id: 6, product: "shoes", price: 2500, count: 10 },
+  { id: 7, product: "watch", price: 4500, count: 10 },
+];
 
 // update
 
@@ -47,11 +47,23 @@
 
 // console.log(product_lessthan_thousand);
 
+// let product_lessthan_five_hundred = product_data.filter(function (
+//   ele,
+//   idx,
+//   array
+// ) {
+//   if (ele.price <= 2000) {
+//     return ele;
+//   }
+// });
+
+// console.log(product_lessthan_five_hundred);
+
 // // reduce returns number
 // // calculation
 
-// let total_price = product_data.reduce(function (acc, ele, array) {
-//   return acc + ele.price;
-// }, 0);
+let total_price = product_data.reduce(function (acc, ele, array) {
+  return acc + ele.price;
+}, 0);
 
-// console.log(total_price);
+console.log(total_price);
